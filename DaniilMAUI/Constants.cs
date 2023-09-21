113c832a8e6b60e318a90456f298a4122905550c
inthespotlight17 <64893014+inthespotlight17@users.noreply.github.com>
2023-09-21 11:28:21 -0700

    DZ230921 - MSAL
    MSAL project. Adds user Registered from Microsoft to its list with name,  email, password = "xxxxxxx", and primeStatus = true

---
+++ DaniilMAUI/Constants.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace DaniilMAUI
+{
+    public static class Constants
+    {
+        //The Application or Client ID will be generated while registering the app in the Azure portal. Copy and paste the GUID.
+        public static readonly string ClientId = "62772b7f-4e57-4a36-beef-877e82e483ed";
+
+        //Leaving the scope to its default values.
+        public static readonly string[] Scopes = new string[] { "openid", "offline_access" };
+
+        //Object ID : 078c8d1b-155c-4920-a436-5ff5a79514c1
+
+        //Tenant ID : 16bde424-ad2c-4bc5-8076-991f37c2cf59
+    }
+}
--- DaniilMAUI/pages/UsersList.xaml.cs
+++ DaniilMAUI/pages/UsersList.xaml.cs
@@ -1,12 +1,17 @@
+using CommunityToolkit.Maui.Alerts;
+using DaniilMAUI.Models;
 using DaniilMAUI.Models.ViewModels;
-
+//using Microsoft.Graph.Models;
+using Microsoft.Identity.Client;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace DaniilMAUI.Pages;
 
 public partial class UsersList : ContentPage
 {
-    private GraphService graphService;
-    private Microsoft.Graph.Models.User user;
+    //private GraphService graphService;
+    //private Microsoft.Graph.Models.User user;
 
 
     public UsersList()
@@ -27,17 +32,92 @@ public partial class UsersList : ContentPage
 
     }
 
-    private async void GetUserInfoBtn_Clicked(object sender, EventArgs e)
+    //private async void GetUserInfoBtn_Clicked(object sender, EventArgs e)
+    //{
+    //    if (graphService == null)
+    //    {
+    //        graphService = new GraphService();
+    //    }
+    //    user = await graphService.GetMyDetailsAsync();
+
+
+    //    HelloLabel.Text = (user != null) ? $"Hello, {user.DisplayName}!" : $"We got an error here! Something with your credentials...";
+
+    //}
+
+    private async void OnLoginClicked(object sender, EventArgs e)
     {
-        if (graphService == null)
+        try
         {
-            graphService = new GraphService();
+            var authService = new AuthService();
+            var result = await authService.LoginAsync(CancellationToken.None);
+            var token = result?.IdToken; // AccessToken also can be used
+            if (token != null)
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var data = handler.ReadJwtToken(token);
+                var claims = data.Claims.ToList();
+                if (data != null)
+                {
+                    var stringBuilder = new StringBuilder();
+
+                    string name = data.Claims.FirstOrDefault(x => x.Type.Equals("name"))?.Value;
+                    string email = data.Claims.FirstOrDefault(x => x.Type.Equals("preferred_username"))?.Value;
+
+                    AddCredentialsToExtUser(name, email);
+
+                    //stringBuilder.AppendLine($"Name: {name}");
+                    //stringBuilder.AppendLine($"Email: {email}");
+
+                    //User user = new User();
+
+                    //user.UserName = name;
+                    //user.Email = email;
+                    //user.Password = "xxxxxxx";
+                    //user.PrimeUser = true;
+
+                    //var userModel = new UserModel { User = user };
+                    //var ViewUserInfo = new ViewUserInfo();
+                    //ViewUserInfo.BindingContext = userModel;
+
+
+
+                    await Toast.Make(stringBuilder.ToString()).Show();
+                }
+            }
         }
-        user = await graphService.GetMyDetailsAsync();
-
-
-        HelloLabel.Text = (user != null) ? $"Hello, {user.DisplayName}!" : $"We got an error here! Something with your credentials...";
-
+        catch (MsalClientException ex)
+        {
+            await Toast.Make(ex.Message).Show();
+        }
     }
+
+    /*
+     * This function uses Name and Email from Microsoft account, which was pulled out by OnLoginClicked()
+     */
+
+    private async void AddCredentialsToExtUser(string name, string email)
+    {
+        User user = new User();
+
+        user.UserName = name;
+        user.Email = email;
+        user.Password = "xxxxxxx";
+        user.PrimeUser = true;
+
+        var userModel = new UserModel { User = user };
+        var ViewUserInfo = new ViewUserInfo();
+        ViewUserInfo.BindingContext = userModel;
+
+        UsersListViewModel usersListViewModel = new UsersListViewModel();
+        usersListViewModel.AllUsers.Add(user);
+
+
+
+        await Navigation.PushAsync(ViewUserInfo);
+    }
+
+
+
 
 }
